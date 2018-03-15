f112722aab30c383199e36a28abc4bf44749bb83
Hongsomang <ghdthakd12@naver.com>
2018-03-15 23:49:21 +0900

    .

--- MEMO/app/src/main/java/com/example/dsm2016/memo/Dialog/Color_Dialog.java
+++ MEMO/app/src/main/java/com/example/dsm2016/memo/Dialog/Color_Dialog.java
@@ -6,11 +6,17 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.support.annotation.NonNull;
+import android.view.View;
 import android.view.Window;
 
 import com.example.dsm2016.memo.R;
 import com.larswerkman.holocolorpicker.ColorPicker;
+import com.larswerkman.holocolorpicker.OpacityBar;
+import com.larswerkman.holocolorpicker.SVBar;
+import com.larswerkman.holocolorpicker.SaturationBar;
+import com.larswerkman.holocolorpicker.ValueBar;
 
+import static android.app.Activity.RESULT_OK;
 import static android.content.Intent.getIntent;
 
 /**
@@ -27,7 +33,29 @@ public class Color_Dialog extends Dialog {
         setContentView(R.layout.activity_color_dialog);
         intent=getIntent();
         int OldColor=intent.getIntExtra("OldColor",0);
+        colorPicker=(ColorPicker)findViewById(R.id.picker);
+        SVBar svBar=(SVBar)findViewById(R.id.svbar);
+        OpacityBar opacityBar=(OpacityBar)findViewById(R.id.opacitybar);
+        SaturationBar saturationBar=(SaturationBar)findViewById(R.id.saturationbar);
+        ValueBar valueBar=(ValueBar)findViewById(R.id.valuebar);
+        colorPicker.addSVBar(svBar);
+        colorPicker.addOpacityBar(opacityBar);
+        colorPicker.addSaturationBar(saturationBar);
+        colorPicker.addValueBar(valueBar);   
 
+        colorPicker.getColor();
 
+        if(OldColor !=0){
+            colorPicker.setOldCenterColor(OldColor);
+
+        }else {
+            colorPicker.setOldCenterColor(colorPicker.getColor());
+        }
+
+    }
+    public  void getColor(View view){
+        intent.putExtra("color",colorPicker.getColor());
+        setResult(RESULT_OK,intent);
+        finish();
     }
 }
