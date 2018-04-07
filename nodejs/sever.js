96e188544f48cdfdb565abf2fa605e9ecfd3b9b2
Hongsomang <ghdthakd12@naver.com>
2018-04-07 14:20:16 +0900

    .

--- nodejs/sever.js
+++
@@ -1,3 +0,0 @@
-var http = require('http'); http.createServer(function (req, res) { res.writeHead(200, {'Content-Type': 'text/plain'}); res.end('Hello World\n'); }).listen(8000, '10.156.145.1'); //서버 아이피와 포트번호를 알맞게 설정 
-console.log('Server running at http://127.0.0.1:8000/'); 
-
